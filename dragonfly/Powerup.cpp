674f479a538b7909d17dba372780ccae18099674
MissingMyHead <missingmyhead@gmail.com>
2014-10-08 00:11:19 -0400

    Powerup types
    Powerups are now randomly either INVINCIBILITY or RAPID_FIRE

--- dragonfly/Powerup.cpp
+++ dragonfly/Powerup.cpp
@@ -1,16 +1,28 @@
 #include "Powerup.h"
+#include <stdlib.h>
 
 Powerup::Powerup() {
 	ResourceManager &resource_manager = ResourceManager::getInstance();
 	Sprite *p_temp_sprite;
-	p_temp_sprite = resource_manager.getSprite("invincibility");
+	
+	int power_type = random()%NUMBER_POWER;
+	
+	if (power_type == 0) {
+		p_temp_sprite = resource_manager.getSprite("invincibility");
+		power = INVINCIBILITY;
+	} 
+	if (power_type == 1) {
+		p_temp_sprite = resource_manager.getSprite("rapid_fire");
+		power = RAPID_FIRE;
+	}
+	
 	if (!p_temp_sprite) {
-    log_manager.writeLog("Powerup::Powerup(): Warning! Sprite '%s' not found", "invincibility");	
+		log_manager.writeLog("Powerup::Powerup(): Warning! Sprite not found");	
 	} else {
 		setSprite(p_temp_sprite);
 		setTransparency();
     }
-	power = INVINCIBILITY;
+	
 	setXVelocity(-1);
 }
 
